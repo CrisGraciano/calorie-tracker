9bd0f09e469fcf4e0be2e4324e665fa525dfc30c
ascosper@hotmail.com <Amaya@Normas-Air.attlocal.net>
2022-10-22 10:02:54 -0500

    login/out/sigup

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,9 +1,9 @@
-async function signupFormHandler(event) {
+async function loginFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
     if (username && email && password) {
       const response = await fetch('/api/users', {
@@ -18,12 +18,12 @@ async function signupFormHandler(event) {
       console.log(response);
     }
   }
-  async function signupFormHandler(event) {
+  async function loginFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username-loginp').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
     if (username && email && password) {
       const response = await fetch('/api/users', {
